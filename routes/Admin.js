321a2796c0b1149149a37d242dae8e0ad48c6389
David Weiland <weiland.david@icloud.com>
2022-02-24 17:38:35 +0100

    add auth

--- routes/Admin.js
+++ routes/Admin.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const adminCtrl = require('../controllers/Admin')
+const auth = require('../middleware/authAdmin')
 
-router.post('/signup', adminCtrl.signup)
+router.post('/signup', auth, adminCtrl.signup)
 router.post('/login', adminCtrl.login)
-router.put('/:id', adminCtrl.modifyAdmin)
+router.put('/:id', auth, adminCtrl.modifyAdmin)
 
 module.exports = router
