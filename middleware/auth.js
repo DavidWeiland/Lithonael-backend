7b6de73b8a6a5fe48d222e3018f2397f80749efa
David Weiland <weiland.david@icloud.com>
2022-02-24 10:59:27 +0100

    modify secret for auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]
-    const decodedToken = jwt.verify(token, process.env.DECODE_TOKEN_ADMIN)
+    const decodedToken = jwt.verify(token, process.env.DECODE_TOKEN_USER)
     const userId = decodedToken.userId
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid User ID'
